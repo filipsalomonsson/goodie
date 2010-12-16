41ec222b02d359c169a15be583df071f6687d9ae
Filip Salomonsson <filip.salomonsson@gmail.com>
2010-12-16 23:17:48 +0100

    Fired up; ready to go!

---
+++ goodie.js
@@ -0,0 +1,12 @@
+/* Goodie.js
+ * By Filip Salomonsson <http://filipsalomonsson.com/>
+ * MIT Licensed.
+ *
+ * Enjoy.
+ */
+
+(function(window, undefined) {
+
+    // I'm working on it, mkay?
+
+})(this);
